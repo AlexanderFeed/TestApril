0075fe6c7305382145202a7f76430964ad895b5e
AlexanderFeed <96307947+AlexanderFeed@users.noreply.github.com>
2025-01-13 00:52:44 +0300

    DoModal

---
+++ components/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="modal-overlay">
+      <div class="modal">
+        <h6>Saved!</h6>
+        <p>kekek</p>
+        <button>Go Home</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+  }
+  </script>
